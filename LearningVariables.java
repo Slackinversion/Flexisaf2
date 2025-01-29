1a52925bb6df21ba68f5ac3b315d5b1b9e12e1c1
Ogbe walter <ogbe5x@gmail.com>
2025-01-29 14:09:21 +0100

    Deliveriable for Week 3

---
+++ LearningVariables.java
@@ -0,0 +1,15 @@
+public class LearningVariables {
+    public static void main(String[] args) {
+        
+        int age = 23;
+        double price = 99.99;
+        boolean isthisboolean = true;
+        String message = " Hello, Flexisaf";
+        
+        // Printing values
+        System.out.println("My Age is: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("flexisaf intern: " + isthisboolean);
+        System.out.println("Message:" + message);
+    }
+}
